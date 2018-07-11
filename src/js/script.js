92356780692526b7b55e52a43c50e55691215a75
Pijoo <bartosz.surzyn@gmail.com>
2018-07-11 17:52:23 +0200

    ver beta 1.0 :)

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+
+$('.slick-slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
